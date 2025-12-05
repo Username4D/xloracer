ad2157acb6bbe9728a3edf72187d695ac06bdf64
Username4D <studio@kremzow.org>
2025-12-05 20:52:33 +0100

    added tileset to track scene map, added tile models, small car physics changes

--- scripts/car.gd
+++ scripts/car.gd
@@ -11,7 +11,9 @@ func _physics_process(delta: float) -> void:
 	
 
 	steering = move_toward(steering, Input.get_axis("ui_left", "ui_right"), delta * 3)
-	speed = move_toward(speed, max_speed, delta * 10) if Input.is_action_pressed("ui_up") else move_toward(speed, 0, delta * 8)
+	speed = move_toward(speed, max_speed, delta * 10) if Input.is_action_pressed("ui_up") else speed
+	speed = move_toward(speed, -max_speed / 2, delta * 8) if Input.is_action_pressed("ui_down") else speed
+	speed = move_toward(speed, 0, delta * 8) if Input.get_axis("ui_up", "ui_down") == 0 else speed
 	
 	var movement = Vector3(int(is_drifting)  * .5 * drift_direction,0,1).normalized().rotated(Vector3(0,1,0),self.rotation.y) * speed
 	velocity.x = movement.x
@@ -25,8 +27,10 @@ func _physics_process(delta: float) -> void:
 	velocity.y -= .2
 	
 	move_and_slide()
-	if speed > 0:
-		if speed < 20:
+	if is_on_wall():
+		speed = move_toward(speed, 0, delta * 32)
+	if abs(speed) > 0:
+		if abs(speed) < 20:
 			self.rotation.y += steering * speed / -20 * delta + (delta * int(is_drifting) * drift_direction * -1)
 		else:
 			self.rotation.y += steering * 20 / -20* delta+ (delta * int(is_drifting) * drift_direction * -1)
